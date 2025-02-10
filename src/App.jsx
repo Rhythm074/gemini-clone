7acdf919db3621fc843faebb2ba4e929eb4e4baa
Rhythm074 <rhythmsansiya03@gmail.com>
2025-02-10 05:58:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Main from './Components/Main/Main'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Sidebar/>
+     <Main/>
+    </>
+  )
+}
+
+export default App    
